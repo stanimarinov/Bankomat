b03f255e694d5324162e145cf5e33f13abdc38b6
Stani Marinov <stani.marinov@yahoo.com>
2023-10-11 14:31:28 +0300

    add

---
+++ clients.py
@@ -0,0 +1,20 @@
+clients = [
+    {"name":"Ivan"  , "pin":"1234" , "balance":1000},
+    {"name":"Petar" , "pin":"2345" ,  "balance":500},
+    {"name":"Todor" , "pin":"3456" ,  "balance":600},
+    {"name":"Asen"  , "pin":"4567" , "balance":1100},
+    {"name":"Marin" , "pin":"5678" ,  "balance":800},
+    {"name":"Iva"   , "pin":"1122" , "balance":1200},
+    {"name":"Maria" , "pin":"3344" ,  "balance":700},
+    {"name":"Neda"  , "pin":"5566" , "balance":1300},
+    {"name":"Bela"  , "pin":"7788" , "balance":1400},
+    {"name":"Vela"  , "pin":"9900" , "balance":1500}
+]
+
+
+comands = [
+    "1. Withdraw" ,
+    "2. Deposit"  ,
+    "3. Balance"  ,
+    "4. Quit"   
+]
